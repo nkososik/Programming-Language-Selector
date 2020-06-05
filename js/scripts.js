aa56e70deee95b4292309bd69dd701c4316f9959
Nikolas Kososik <nikolas.kososik@gmail.com>
2020-06-05 15:38:44 -0700

    adjust results and add .hide to scripts.js to toggle results in and out as well as change fadeToggle to fadeIn

--- js/scripts.js
+++ js/scripts.js
@@ -10,14 +10,29 @@ $(document).ready(function() {
     if (age) {
       if (cheese === "on") {
         $("#csharp").fadeIn();
+        $("#javascript").hide();
+        $("#python").hide();
+        $("#ruby").hide();
       } else if (age <= 30 && gender === "Non-Binary") {
         $("#ruby").fadeIn();
+        $("#javascript").hide();
+        $("#python").hide();
+        $("#csharp").hide();
       } else if (age >= 30 && age < 45) {
         $("#python").fadeIn();
+        $("#javascript").hide();
+        $("#csharp").hide();
+        $("#ruby").hide();
       } else if (age >= 45 && gender === "Male") {
         $("#javascript").fadeIn();
+        $("#csharp").hide();
+        $("#python").hide();
+        $("#ruby").hide();
       } else if (gender === "Female") {
         $("#javascript").fadeIn();
+        $("#csharp").hide();
+        $("#python").hide();
+        $("#ruby").hide();
       }
     } else {
       alert("Please enter your age")
