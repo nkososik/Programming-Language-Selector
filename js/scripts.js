72539f0f09d28f498f4107c0dbe7fe568b59fdff
Nikolas Kososik <nikolas.kososik@gmail.com>
2020-06-05 11:07:03 -0700

    create const variables in scripts.js relating to form inputs

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
   $("form#form1").submit(function(event) {
+    const age = parseInt($("input#age").val());
+    const gender = $("select#gender").val();
+    const operator = $("input:radio[name=operator]:checked").val();
+    const cheese = $("#defaultCheck1").val();
 
     event.preventDefault();
   });
